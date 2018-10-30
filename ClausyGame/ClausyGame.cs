9c0188716474a0eb2be288f648d401c9860c6647
Andy Marquez <bobii144@gmail.com>
2018-10-29 19:58:14 -0700

    Init

--- ClausyGame/ClausyGame.cs
+++ ClausyGame/ClausyGame.cs
@@ -7,12 +7,12 @@ namespace ClausyGame
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class ClausyGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public ClausyGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- ClausyGame/Program.cs
+++ ClausyGame/Program.cs
@@ -14,7 +14,7 @@ namespace ClausyGame
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ClausyGame())
                 game.Run();
         }
     }
